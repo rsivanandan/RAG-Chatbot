efc9fd3deaa9076d565cb74ae0519070dd1b4bd7
rsivanandan <34827144+rsivanandan@users.noreply.github.com>
2024-06-20 21:27:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from llama_index.llms.ollama import Ollama
 from llama_index.core.chat_engine.types import ChatMode
+import pysqlite3
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb
 import streamlit as st
 from pathlib import Path
