46754a8b50c861dbeb36d96e68f520aa70fb90be
rsivanandan <34827144+rsivanandan@users.noreply.github.com>
2024-06-20 21:02:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,129 @@
+from llama_index.llms.ollama import Ollama
+from llama_index.core.chat_engine.types import ChatMode
+import chromadb
+import streamlit as st
+from pathlib import Path
+from rag_functions import (
+    Create_Vector,
+    init_llm_ollama,
+    init_llm_hf,
+    init_index,
+)
+import shutil
+import os
+    
+st.set_page_config(
+    page_title="RAG Chatbot",
+    page_icon="./images/llama.png",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+    
+def Clear_Chat() -> None:
+    keys = list(st.session_state.keys())
+    for key in keys:
+        st.session_state.pop(key)
+
+
+def main() -> None:
+    menu = ["Document Ingestion", "Chatbot"]
+    # st.sidebar.image("./images/llama.png", use_column_width=True)
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Chatbot":
+        st.title("RAG Chat Bot")
+        llmtype = st.radio(
+            "Choose your LLM",
+            [
+                "Ollama llama3 (local)",
+                "Huggingface Mistral",
+            ],
+            horizontal=True,
+        )
+        if llmtype == "Ollama llama3 (local)":
+            st.sidebar.write(
+                "*Ensure that you're running Ollama locally and also have downloaded the llama3 model*"
+            )
+            init_llm_ollama()
+            index = init_index()
+        elif llmtype == "Huggingface Mistral":
+            HF_Token = st.sidebar.text_input("Huggingface Token", type="password")
+            st.sidebar.write(
+                "*Sigup for a free account at https://huggingface.co/. Create a free API key and input above*"
+            )
+            init_llm_hf("mistralai/Mistral-7B-Instruct-v0.3", HF_Token)
+            index = init_index()
+        if "chat_engine" not in st.session_state.keys():
+            st.session_state.chat_engine = index.as_chat_engine(
+                chat_mode="condense_question", streaming=True, verbose=True
+                )
+        if "messages" not in st.session_state.keys():
+            st.session_state.messages = [
+                {
+                "role": "assistant",
+                "content": "Ask me anything",
+                }
+            ]
+        if prompt := st.chat_input("Ask me a question"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = st.session_state.chat_engine.chat(message=prompt)
+                    st.write(response.response)
+                    message = {"role": "assistant", "content": response.response}
+                    st.session_state.messages.append(message)
+
+    elif choice == "Document Ingestion":
+        st.subheader("Document Ingestion")
+        with st.form("Cleanup"):
+            clean = st.form_submit_button("Reinitialize")
+            st.write("This helps remove all previous data & chat history")
+            if clean:
+                try:
+                    shutil.rmtree("./data")
+                    os.makedirs("data")
+                    Clear_Chat()
+                    # load_client = chromadb.PersistentClient(path="./chroma_db")
+                    # load_client.clear_system_cache
+                    st.success("Successfully Reinitialized")
+                except:
+                    os.makedirs("data")
+                    st.error("Nothing to initialize")
+        with st.form(key="FileUpload", clear_on_submit=True):
+            uploaded_file = st.file_uploader(
+                "Drop your files here", type=["txt", "csv", "pdf", "py"]
+            )
+            Submit = st.form_submit_button(label="Save")
+            if Submit:
+                st.markdown("**The file is sucessfully Saved.**")
+                # Save uploaded file to './data' folder.
+                save_folder = "./data"
+                save_path = Path(save_folder, uploaded_file.name)
+                with open(save_path, mode="wb") as f:
+                    f.write(uploaded_file.getvalue())
+        if uploaded_file is not None:
+            select_rag = st.radio(
+                "Select RAG Type",
+                ["Vector Database", "Knowledge Graph"],
+                horizontal=True,
+            )
+            if select_rag == "Knowledge Graph":
+                st.info("Not Implemented Yet")
+            vectorized = st.button("Embed & Vectorize")
+            if vectorized:
+                Clear_Chat()
+                if select_rag == "Vector Database":
+                    with st.spinner("Please wait while I vectorize..."):
+                        Create_Vector()
+                        st.success("Done...You can launch the Chatbot now")
+                elif select_rag == "Graph Database":
+                    st.info ("Not Implemented Yet")
+
+
+if __name__ == "__main__":
+    main()
---
+++ rag_functions.py
@@ -0,0 +1,107 @@
+# All the RAG - Document Ingestion & Retrieval code
+
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    Settings,
+)
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+from llama_index.llms.huggingface import HuggingFaceInferenceAPI
+import chromadb
+import streamlit as st
+
+@st.cache_resource(show_spinner=False)
+def init_llm_hf(model_name, HF_token) -> None:
+
+    llm = HuggingFaceInferenceAPI(model_name=model_name, token=HF_token)
+    embed_model = HuggingFaceEmbedding(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+
+
+@st.cache_resource(show_spinner=False)
+def init_llm_ollama():
+    llm = Ollama(model="llama3", request_timeout=600.0)
+
+    # define embedding function
+
+    embed_model = HuggingFaceEmbedding(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+
+
+@st.cache_resource(show_spinner=False)
+def init_index() -> VectorStoreIndex:
+    # Load from disk
+
+    load_client = chromadb.PersistentClient(path="./chroma_db")
+
+    # Fetch the collection
+    chroma_collection = load_client.get_collection("tsr_collection")
+
+    # Fetch the vector store
+
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    # Get the index from the vector store
+
+    return VectorStoreIndex.from_vector_store(vector_store)
+
+
+@st.cache_resource(show_spinner=False)
+def Create_Vector() -> None:
+
+    # create client and a new collection
+
+    chroma_client = chromadb.EphemeralClient()
+
+    chroma_client.get_or_create_collection(name="tsr_collection")
+    chroma_client.delete_collection("tsr_collection")
+
+    embed_model = HuggingFaceEmbedding(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+
+    # load documents
+    documents = SimpleDirectoryReader("./data").load_data()
+
+    # save vector & save to disk
+
+    db = chromadb.PersistentClient(path="./chroma_db")
+    chroma_collection = db.get_or_create_collection("tsr_collection")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context, embed_model=embed_model
+    )
+
+
+"""def Create_Graph() -> None:
+    llm = None
+    # load documents
+    documents = SimpleDirectoryReader("./data").load_data()
+    embed_model = HuggingFaceEmbedding(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+        )
+    #setup the storage context
+    graph_store = SimpleGraphStore()
+    storage_context = StorageContext.from_defaults(graph_store=graph_store)
+    #Construct the Knowlege Graph Undex
+    index = KnowledgeGraphIndex.from_documents( documents=documents,
+                                           max_triplets_per_chunk=3,
+                                           storage_context=storage_context,
+                                           embed_model=embed_model,
+                                           include_embeddings=True)
+    index.persist(persist_path="knowledge_graph.json")
+    
+def init_graph() -> None:
+    pass
+"""
